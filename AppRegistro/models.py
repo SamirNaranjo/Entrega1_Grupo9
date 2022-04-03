cf21049885faa34e943b485a62c26c4253c6281f
Tomas Chirieleison <tomaschirieleison2015@gmail.com>
2022-04-03 12:43:47 -0300

    Clases comentadas

--- AppRegistro/models.py
+++ AppRegistro/models.py
@@ -1,6 +1,22 @@
+from typing_extensions import Self
+from urllib import request
 from django.db import models
 
 # Create your models here.
-class Persona(models.Model):
+class Usuario(models.Model):
     nombre = models.CharField('nombre', max_length=50)
-    apellido = models.CharField('apellido', max_length=50)
+    id_usuario = models.CharField('apellido', max_length=50)
+    contraseña = models.CharField('contraseña', max_length=50)
+ 
+class Producto():
+    #nombre_producto = 
+    #cantidad_producto =
+    #precio_producto = 
+
+class Pedido():
+    #numero_pedido =
+
+
+class Catalogo_producto():
+    #mostrar_producto #metodo
+    
