d50a3514a80643ae161357524769c7f1f2cedcb0
gromero <ingromerogenesis@gmail.com>
2022-04-04 15:01:24 -0300

    se agrego clase usuario

--- AppRegistro/models.py
+++ AppRegistro/models.py
@@ -1,4 +1,4 @@
-from typing_extensions import Self
+
 from urllib import request
 from django.db import models
 
@@ -6,17 +6,4 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField('nombre', max_length=50)
     id_usuario = models.CharField('apellido', max_length=50)
-    contraseña = models.CharField('contraseña', max_length=50)
- 
-class Producto():
-    #nombre_producto = 
-    #cantidad_producto =
-    #precio_producto = 
-
-class Pedido():
-    #numero_pedido =
-
-
-class Catalogo_producto():
-    #mostrar_producto #metodo
-    
+    password= models.CharField('password', max_length=50)
