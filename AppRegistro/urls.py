3e795a07f340f83b78412def67a08f449e333997
gromero <ingromerogenesis@gmail.com>
2022-04-05 14:41:30 -0300

    Se realizo temple usuario

--- AppRegistro/urls.py
+++ AppRegistro/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import  inicio
+from .views import usuario
 
 urlpatterns = [
     path('', inicio),
     # path('personas/', personas),
+    path('usuario/' , usuario ),
+    
     
 ]
