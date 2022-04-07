5518fde073f462bbc4dfbd28aa5d2abe83e1eccc
gromero <ingromerogenesis@gmail.com>
2022-04-07 16:12:30 -0300

    herencia HTML

--- AppRegistro/views.py
+++ AppRegistro/views.py
@@ -14,8 +14,19 @@ def inicio(request):
 #     return render(request, 'Appcoder/personas.html')
 
 def usuario(request):
+    if request.method == 'POST':
+        usurioFormulario = usuario_formulario(request.POST)
+        #print(usurioFormulario)
+        if usurioFormulario.is_valid():
+            informacion = usurioFormulario.cleaned_data
+            persona = Usuario(nombre = informacion['nombre'], id_usuario = informacion['id_usuario'], contraseña= informacion['contraseña'])
+            persona.save()
+            return render(request, 'AppRegistro/inicio.html')
+        
 
-    return render(request, 'AppRegistro/usuario.html') 
+    else:
+        usurioFormulario = usuario_formulario()
+        return render (request, 'AppRegistro/usuario.html', {'miForm':usurioFormulario})
 
 def pedido (request):
 
